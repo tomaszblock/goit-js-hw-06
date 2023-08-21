3038602cdaee9935dd8650860b22aa9e0c52c93c
tomaszblock <blocktomasz@gmail.com>
2023-08-21 20:11:37 +0200

    hw1 done

--- js/task-01.js
+++ js/task-01.js
@@ -1,8 +1,35 @@
 const categories = document.querySelector("#categories");
 const numberOfCategories = categories.children.length;
+const category = document.querySelectorAll("h2");
 
 console.log(`Number of categories: ${numberOfCategories}`);
 console.log("");
-console.log(`Category: ${categories.firstElementChild.value}`);
-
-console.log(categories.querySelectorAll("li"));
+console.log(`Category: ${category[0].textContent}`);
+console.log(
+  `Elements: ${
+    document
+      .querySelectorAll(".item")[0]
+      .querySelector("ul")
+      .querySelectorAll("li").length
+  }`
+);
+console.log("");
+console.log(`Category: ${category[1].textContent}`);
+console.log(
+  `Elements: ${
+    document
+      .querySelectorAll(".item")[1]
+      .querySelector("ul")
+      .querySelectorAll("li").length
+  }`
+);
+console.log("");
+console.log(`Category: ${category[2].textContent}`);
+console.log(
+  `Elements: ${
+    document
+      .querySelectorAll(".item")[2]
+      .querySelector("ul")
+      .querySelectorAll("li").length
+  }`
+);
