f34ef88cca95467ff1673ee49277df3baea3860e
tomaszblock <blocktomasz@gmail.com>
2023-08-21 20:55:18 +0200

    hw2 done

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+for (const i of ingredients) {
+  console.log(i);
+  const parent = document.querySelector("#ingredients");
+  const newElement = document.createElement("li");
+  newElement.classList.add("item");
+  newElement.textContent = i;
+  parent.append(newElement);
+}
