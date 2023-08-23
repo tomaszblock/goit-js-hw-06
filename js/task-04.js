5c199288365be4631ab1ee56f681f891437e3a55
tomaszblock <blocktomasz@gmail.com>
2023-08-23 21:16:04 +0200

    add hw4

--- js/task-04.js
+++ js/task-04.js
@@ -2,15 +2,13 @@ const value = document.querySelector("#value");
 const buttonInc = document.querySelector('button[data-action="increment"]');
 const buttonDec = document.querySelector('button[data-action="decrement"]');
 let counterValue = 0;
-console.log(value);
 const add = () => {
-  //   console.log(counterValue);
-  return counterValue++;
+  counterValue++;
+  return (value.textContent = `${counterValue}`);
 };
 const subtract = () => {
-  //   console.log(counterValue);
-  return counterValue--;
+  counterValue--;
+  return (value.textContent = `${counterValue}`);
 };
 buttonInc.addEventListener("click", add);
 buttonDec.addEventListener("click", subtract);
-console.log(`teraz liczba to ${counterValue}`); // <--- czemu tu nie odświeża counterValue?
