d90e9be71abcda0d27aafab7263c9d53260ea7b8
tomaszblock <127046228+tomaszblock@users.noreply.github.com>
2023-08-25 15:49:56 +0200

    add hw8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,27 @@
+const registerForm = document.querySelector(".login-form")
+const emailInput = document.querySelector("input[type=email]")
+const passwordInput = document.querySelector("input[type=password]")
+registerForm.addEventListener("submit", validation);
+const result = {}
+
+
+
+function validation(event) {
+event.preventDefault();
+const form = event.currentTarget;
+const email = form.elements.email.value;
+const password = form.elements.password.value
+if (email === "" || password === "" ) {
+//wiem, że chodziło tu o bład w konsoli console.log("text") ale chciałem poexperymentować :P
+// nie wiedziałem tylko jak kolor placeholdera zmienić poprzez JSa
+emailInput.setAttribute("placeholder", "Fill it!")
+passwordInput.setAttribute("placeholder", "Fill it!")
+}
+result.email = email;
+result.password = password;
+console.log(result);
+form.reset();
+} 
+
+
 
