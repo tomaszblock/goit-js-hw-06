15f2e41e7bdc08deee439218e862bc7359b91f85
tomaszblock <127046228+tomaszblock@users.noreply.github.com>
2023-08-25 10:31:24 +0200

    Update task-05.js
    add hw5

---
+++ js/task-05.js
@@ -0,0 +1,11 @@
+const textInput = document.querySelector("#name-input");
+const output = document.querySelector("#name-output");
+textInput.addEventListener("input", (event) => {
+const value = event.currentTarget.value;
+if (value.length === 0) {
+   output.innerText = "Anonymous"
+}
+else {
+    output.textContent = event.currentTarget.value;
+}
+  });
