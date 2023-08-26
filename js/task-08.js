ad2264dc878214ba769696c687ed82a1682715b5
tomaszblock <blocktomasz@gmail.com>
2023-08-26 12:04:54 +0200

    add hw9

--- js/task-08.js
+++ js/task-08.js
@@ -1,27 +1,23 @@
-const registerForm = document.querySelector(".login-form")
-const emailInput = document.querySelector("input[type=email]")
-const passwordInput = document.querySelector("input[type=password]")
+const registerForm = document.querySelector(".login-form");
+const emailInput = document.querySelector("input[type=email]");
+const passwordInput = document.querySelector("input[type=password]");
 registerForm.addEventListener("submit", validation);
-const result = {}
-
-
+const result = {};
 
 function validation(event) {
-event.preventDefault();
-const form = event.currentTarget;
-const email = form.elements.email.value;
-const password = form.elements.password.value
-if (email === "" || password === "" ) {
-//wiem, że chodziło tu o bład w konsoli console.log("text") ale chciałem poexperymentować :P
-// nie wiedziałem tylko jak kolor placeholdera zmienić poprzez JSa
-emailInput.setAttribute("placeholder", "Fill it!")
-passwordInput.setAttribute("placeholder", "Fill it!")
+  event.preventDefault();
+  const form = event.currentTarget;
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+  if (email === "" || password === "") {
+    //wiem, że chodziło tu o bład w konsoli console.log("text") ale chciałem poeksperymentować :P
+    // nie wiedziałem tylko jak kolor placeholdera zmienić poprzez JSa
+    emailInput.setAttribute("placeholder", "Fill it!");
+    passwordInput.setAttribute("placeholder", "Fill it!");
+  } else {
+    result.email = email;
+    result.password = password;
+    console.log(result);
+    form.reset();
+  }
 }
-result.email = email;
-result.password = password;
-console.log(result);
-form.reset();
-} 
-
-
-
--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,15 @@
+const changeColor = document.querySelector("button");
+const textValue = document.querySelector("span");
+const body = document.querySelector("body");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+changeColor.addEventListener("click", () => {
+  const color = getRandomHexColor();
+  textValue.textContent = color;
+  body.style.backgroundColor = color;
+});
