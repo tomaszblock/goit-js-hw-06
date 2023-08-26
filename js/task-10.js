1821f7ac57b6481d1c14a52490e6dadcad7a9dbf
tomaszblock <blocktomasz@gmail.com>
2023-08-26 14:39:38 +0200

    add hw9

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,42 @@
+const inputWindow = document.querySelector("input");
+const createButton = document.querySelector("button[data-create]");
+const destroyButton = document.querySelector("button[data-destroy]");
+const box = document.querySelector("#boxes");
+createButton.addEventListener("click", createBoxes);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+
+function createBoxes() {
+  const amount = inputWindow.value;
+  const firstBox = document.createElement("div");
+  let border = 30;
+  if (amount > 0) {
+    firstBox;
+    firstBox.style.backgroundColor = getRandomHexColor();
+    firstBox.style.width = "30px";
+    firstBox.style.height = "30px";
+    // firstBox.setAttribute("data-id", "1");
+    box.append(firstBox);
+  }
+  if (amount > 1) {
+    for (let i = 1; i < amount; i++) {
+      const nextBox = document.createElement("div");
+      // const test1 = document.querySelector('[data-id="' + selector + '"]');
+
+      border += 10;
+      nextBox;
+      nextBox.style.backgroundColor = getRandomHexColor();
+      nextBox.style.width = border + "px";
+      nextBox.style.height = border + "px";
+      box.append(nextBox);
+
+      // console.log(test2);
+      // console.log(test3);
+      // console.log(test4);
+    }
+  }
 }
