684533bca7bb2e9010ae04fdc7533e5035aed27e
Tomasz Block <blocktomasz@gmail.com>
2023-08-30 15:37:49 +0200

    fixing hw10

--- js/task-10.js
+++ js/task-10.js
@@ -3,6 +3,7 @@ const createButton = document.querySelector("button[data-create]");
 const destroyButton = document.querySelector("button[data-destroy]");
 const box = document.querySelector("#boxes");
 createButton.addEventListener("click", createBoxes);
+let result = 0;
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
@@ -14,13 +15,14 @@ function createBoxes() {
   const amount = inputWindow.value;
   const firstBox = document.createElement("div");
   let border = 30;
-  if (amount > 0) {
+  if (amount > 0 && result === 0) {
     firstBox;
     firstBox.style.backgroundColor = getRandomHexColor();
     firstBox.style.width = "30px";
     firstBox.style.height = "30px";
     // firstBox.setAttribute("data-id", "1");
     box.append(firstBox);
+    result++
   }
   if (amount > 1) {
     for (let i = 1; i < amount; i++) {
@@ -33,6 +35,8 @@ function createBoxes() {
       nextBox.style.width = border + "px";
       nextBox.style.height = border + "px";
       box.append(nextBox);
+      result++;
+      console.log(result)
 
       // console.log(test2);
       // console.log(test3);
@@ -40,3 +44,5 @@ function createBoxes() {
     }
   }
 }
+
+
