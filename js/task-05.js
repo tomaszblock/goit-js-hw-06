bfbb4d364ec008050e33ccc1f4153515f5753e13
tomaszblock <127046228+tomaszblock@users.noreply.github.com>
2024-01-28 10:11:06 +0100

    fixes

--- js/task-05.js
+++ js/task-05.js
@@ -1,11 +1,10 @@
 const textInput = document.querySelector("#name-input");
 const output = document.querySelector("#name-output");
 textInput.addEventListener("input", (event) => {
-const value = event.currentTarget.value;
-if (value.length === 0) {
-   output.innerText = "Anonymous"
-}
-else {
-    output.textContent = event.currentTarget.value;
-}
-  });
+  const value = event.currentTarget.value.trim();
+  if (value.length === 0) {
+    output.innerText = "Anonymous";
+  } else {
+    output.textContent = event.currentTarget.value.trim();
+  }
+});
--- js/task-06.js
+++ js/task-06.js
@@ -1,19 +1,18 @@
-const textInput = document.querySelector("#validation-input")
-const minLength = textInput.dataset.length
-console.log(length)
-
-
+const textInput = document.querySelector("#validation-input");
+const minLength = parseInt(textInput.dataset.length);
+console.log(length);
 
 textInput.addEventListener("blur", (event) => {
-const value = event.currentTarget.value
-if(value.length >= minLength) {
-    textInput.classList.remove("invalid")
-    textInput.classList.add("valid")
-console.log(textInput)
-}
-else {
-    textInput.classList.remove("valid")
-    textInput.classList.add("invalid")
-console.log(textInput)
-}
-})  
+  const value = event.currentTarget.value;
+  if (value.length === minLength) {
+    textInput.classList.remove("invalid");
+    textInput.classList.add("valid");
+    console.log(textInput);
+  } else {
+    textInput.classList.remove("valid");
+    textInput.classList.add("invalid");
+    console.log(textInput);
+  }
+});
+
+console.log(minLength);
